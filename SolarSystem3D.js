28c2b28de26f165df6168616e9624ae0faa15652
Alberto Boffi <alberto2.boffi@mail.polimi.it>
2023-07-30 18:30:20 +0200

    feat: create first version of the application

--- SolarSystem3D.js
+++ SolarSystem3D.js
@@ -2,36 +2,112 @@ import * as THREE from 'three';
 
 class SolarSystem3D{
 
+    #scene; #renderer;
+    #camera;
+
     #initScene(){
         
-        const scene = new THREE.Scene();
+        this.#scene = new THREE.Scene();
 
-        const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        document.body.appendChild(renderer.domElement);
+        this.#renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.#renderer.setSize(window.innerWidth, window.innerHeight);
+        this.#renderer.setPixelRatio(window.devicePixelRatio * 1.5);
+        
+        document.body.appendChild(this.#renderer.domElement);
 
     }
 
-    #setProjectionMatrix(){
-        
-        const fovy = 90;
-        const ar = window.innerWidth / window.innerHeight;
-        const nearPlane = 0.1;
-        const farPlane = 1000;
+    #loadModels(){
 
-        const camera = new THREE.PerspectiveCamera(
+        // load textures
+
+        const suTe = new THREE.TextureLoader().load("textures/sun.jpg");
+        const suMat = new THREE.MeshBasicMaterial({ map: suTe });
+
+        const meTe = new THREE.TextureLoader().load("textures/planets/mercury.jpg");
+        const meMat = new THREE.MeshBasicMaterial({ map: meTe });
+
+        const veTe = new THREE.TextureLoader().load("textures/planets/venus.jpg");
+        const veMat = new THREE.MeshBasicMaterial({ map: veTe });
+
+        const eaTe = new THREE.TextureLoader().load("textures/planets/earth.jpg");
+        const eaMat = new THREE.MeshBasicMaterial({ map: eaTe });
+
+        const maTe = new THREE.TextureLoader().load("textures/planets/mars.jpg");
+        const maMat = new THREE.MeshBasicMaterial({ map: maTe });
+
+        const juTe = new THREE.TextureLoader().load("textures/planets/jupiter.jpg");
+        const juMat = new THREE.MeshBasicMaterial({ map: juTe });
+        
+        const saTe = new THREE.TextureLoader().load("textures/planets/saturn.jpg");
+        const saMat = new THREE.MeshBasicMaterial({ map: saTe });
+
+        const urTe = new THREE.TextureLoader().load("textures/planets/uranus.jpg");
+        const urMat = new THREE.MeshBasicMaterial({ map: urTe });
+
+        const neTe = new THREE.TextureLoader().load("textures/planets/neptune.jpg");
+        const neMat = new THREE.MeshBasicMaterial({ map: neTe });
+
+        // create mesh
+
+        const sphereRadius = 1, sphereXSegments = 100, sphereYSegments = 30;
+        const sphere = new THREE.SphereGeometry(sphereRadius, sphereXSegments, sphereYSegments);
+
+        // create models
+        
+        const sun = new THREE.Mesh(sphere, suMat);
+        const mercury = new THREE.Mesh(sphere, meMat);
+        const venus = new THREE.Mesh(sphere, veMat);
+        const earth = new THREE.Mesh(sphere, eaMat);
+        const mars = new THREE.Mesh(sphere, maMat);
+        const jupiter = new THREE.Mesh(sphere, juMat);
+        const saturn = new THREE.Mesh(sphere, saMat);
+        const neptune = new THREE.Mesh(sphere, neMat);
+        const uranus = new THREE.Mesh(sphere, urMat);
+
+        this.#scene.add(sun);
+        this.#scene.add(mercury);
+        this.#scene.add(venus);
+        this.#scene.add(earth);
+        this.#scene.add(mars);
+        this.#scene.add(jupiter);
+        this.#scene.add(saturn);
+        this.#scene.add(neptune);
+        this.#scene.add(uranus);
+
+    }
+
+    #setViewProjection(){
+        
+        const   fovy = 90,
+                ar = window.innerWidth / window.innerHeight,
+                nearPlane = 0.1,
+                farPlane = 1000;
+
+        this.#camera = new THREE.PerspectiveCamera(
             fovy,
             ar,
             nearPlane,
             farPlane
         );
 
+        this.#camera.position.set(0, 0, 5);
+
+    }
+
+    #animate(){
+
+        requestAnimationFrame(this.#animate.bind(this));
+	    this.#renderer.render(this.#scene, this.#camera);
+        
     }
 
     run(){
 
         this.#initScene();
-        this.#setProjectionMatrix();
+        this.#loadModels();
+        this.#setViewProjection();
+        this.#animate();
 
     }
 
@@ -41,5 +117,7 @@ function main(){
 
     const app = new SolarSystem3D();
     app.run();
-    
+
 }
+
+main()
